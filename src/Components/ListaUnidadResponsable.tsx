acacfcadb49446762a6af07fb9e75b19818c1ed5
RobertDLR03 <lobelto2003@gmail.com>
2025-04-18 00:31:59 -0600

    Update ListaUnidadResponsable.tsx

--- src/Components/ListaUnidadResponsable.tsx
+++ src/Components/ListaUnidadResponsable.tsx
@@ -1,1 +1,28 @@
-
+import { useState, useEffect } from 'react';
+import { DataTable } from 'primereact/datatable';
+import { Column } from 'primereact/column';
+import UnidadResponsableService from '../Services/UnidadResponsableService';
+interface UnidadResponsable {
+    idUnidadResponsable: number;
+    jefeUnidad: string;
+    numeroUnidadResponsable: number;
+}
+export default function ListaUnidadResponsable() {
+    const [unidadResponsable, setUnidadResponsable] = useState<UnidadResponsable[]>([]);
+    useEffect(() => {
+       UnidadResponsableService.findAll().then(Response => {
+            setUnidadResponsable(Response.data);
+        }).catch(error => {
+            console.log(error);
+        })
+    }, []);
+    return (
+        <div className="card">
+            <DataTable value={unidadResponsable} tableStyle={{ minWidth: '50rem' }}>
+                <Column field='idUnidadResponsable' header="ID Unidad Responsable"></Column>
+                <Column field='jefeUnidad' header="Jefe Unida"></Column>
+                <Column field='numeroUnidadResponsable' header="Numero Unidad Responsable"></Column>
+            </DataTable>
+        </div>
+    );
+}
