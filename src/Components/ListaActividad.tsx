063135783b25087c8af16ab0da628e9fe7b9ec28
RobertDLR03 <lobelto2003@gmail.com>
2025-04-18 00:02:05 -0600

    Update ListaActividad.tsx

--- src/Components/ListaActividad.tsx
+++ src/Components/ListaActividad.tsx
@@ -1,1 +1,30 @@
-
+import { useState, useEffect } from 'react';
+import { DataTable } from 'primereact/datatable';
+import { Column } from 'primereact/column';
+import PlanificacionService from '../Services/PlanificacionService';
+interface Planificacion {
+    idPlanificacion: number;
+    jefeUnidad: string;
+    numeroActividades: string;
+    objetivoArea: string;
+}
+export default function ListaPlanificacion() {
+    const [planificacion, setPlanificacion] = useState<Planificacion[]>([]);
+    useEffect(() => {
+       PlanificacionService.findAll().then(Response => {
+            setPlanificacion(Response.data);
+        }).catch(error => {
+            console.log(error);
+        })
+    }, []);
+    return (
+        <div className="card">
+            <DataTable value={planificacion} tableStyle={{ minWidth: '50rem' }}>
+                <Column field='idPlanificacion' header="ID"></Column>
+                <Column field='jefeUnidad' header="Jefe Unida"></Column>
+                <Column field='numeroActividades' header="Numero Actividades"></Column>
+                <Column field='objetivoArea' header="Objetivo Area"></Column>
+            </DataTable>
+        </div>
+    );
+}
