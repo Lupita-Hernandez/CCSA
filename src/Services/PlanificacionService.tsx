9a03267dca0ff511e44f186b8c18b5a43fc7577b
RobertDLR03 <lobelto2003@gmail.com>
2025-04-17 23:55:07 -0600

    Update PlanificacionService.tsx

--- src/Services/PlanificacionService.tsx
+++ src/Services/PlanificacionService.tsx
@@ -1,31 +1,21 @@
 import axios from "axios";
-
 const URL_BASE = "http://localhost:8080/planificacion";
-
 class PlanificacionService{
 
-    //metodo para obtener todos los ariculos
     findAll(){
         return axios.get(URL_BASE);
     }
-
     findUsuarioById(idPlanificacion: number){
         return axios.get(URL_BASE + '/' + idPlanificacion);
     }
-    //Metodo para crear una nueva mascota
     create(planificacion: object){
         return axios.post(URL_BASE, planificacion);
     }
-
-    //Metodo para actualizar un alrticulo existente
     update(idPlanificacion: number, planificacion: object){
-        return axios.put(URL_BASE + '/'+idPlanificacion, planificacion)
+        return axios.put(URL_BASE + '/'+idPlanificacion, planificacion);
     }
-
-    //metodo para eliminar un articulo
     delete(idPlanificacion: number){
-        return axios.delete(URL_BASE + '/' + idPlanificacion)
+        return axios.delete(URL_BASE + '/' + idPlanificacion);
     }
 }
-//se exporta la clase 
 export default new PlanificacionService();
